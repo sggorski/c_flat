9fdd7355ed564f28dd309e93002d1da75caaa158
pavlvs-91 <gleindekp@gmail.com>
2025-03-26 22:26:51 +0100

    Notes and key mapping done

--- src/main/java/pl/edu/agh/Key.java
+++ src/main/java/pl/edu/agh/Key.java
@@ -1,4 +1,6 @@
 package pl.edu.agh;
 
 public enum Key {
+    C, Cs, D, Eb, E, F, Fs, G, Ab, A, B, H,
+    Cm, Csm, Dm, Ebm, Em, Fm, Fsm, Gm, Gsm, Am, Bm, Hm
 }
--- src/main/java/pl/edu/agh/Note.java
+++ src/main/java/pl/edu/agh/Note.java
@@ -1,4 +1,11 @@
 package pl.edu.agh;
 
 public enum Note {
+    Cm2, Dm2, Em2, Fm2, Gm2, Am2, Hm2, Bm2, Csm2, Dsm2, Esm2, Fsm2, Gsm2, Asm2, Hsm2, Bsm2, Cbm2, Dbm2, Ebm2, Fbm2, Gbm2, Abm2, Hbm2, Bbm2,
+    Cm1, Dm1, Em1, Fm1, Gm1, Am1, Hm1, Bm1, Csm1, Dsm1, Esm1, Fsm1, Gsm1, Asm1, Hsm1, Bsm1, Cbm1, Dbm1, Ebm1, Fbm1, Gbm1, Abm1, Hbm1, Bbm1,
+    C0, D0, E0, F0, G0, A0, H0, B0, Cs0, Ds0, Es0, Fs0, Gs0, As0, Hs0, Bs0, Cb0, Db0, Eb0, Fb0, Gb0, Ab0, Hb0, Bb0,
+    C1, D1, E1, F1, G1, A1, H1, B1, Cs1, Ds1, Es1, Fs1, Gs1, As1, Hs1, Bs1, Cb1, Db1, Eb1, Fb1, Gb1, Ab1, Hb1, Bb1,
+    C2, D2, E2, F2, G2, A2, H2, B2, Cs2, Ds2, Es2, Fs2, Gs2, As2, Hs2, Bs2, Cb2, Db2, Eb2, Fb2, Gb2, Ab2, Hb2, Bb2,
+    C3, D3, E3, F3, G3, A3, H3, B3, Cs3, Ds3, Es3, Fs3, Gs3, As3, Hs3, Bs3, Cb3, Db3, Eb3, Fb3, Gb3, Ab3, Hb3, Bb3,
+    C4, D4, E4, F4, G4, A4, H4, B4, Cs4, Ds4, Es4, Fs4, Gs4, As4, Hs4, Bs4, Cb4, Db4, Eb4, Fb4, Gb4, Ab4, Hb4, Bb4
 }
