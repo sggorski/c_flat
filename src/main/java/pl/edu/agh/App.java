0d43ac2502f80894668f7c398c7e43e668494efd
pavlvs-91 <gleindekp@gmail.com>
2025-04-25 01:42:52 +0200

    Vivaldi's Spring standard version

--- src/main/java/pl/edu/agh/App.java
+++ src/main/java/pl/edu/agh/App.java
@@ -21,7 +21,7 @@ public class App
         CharStream stream;
         try {
             // stream = CharStreams.fromFileName(String.format("src/main/java/pl/edu/agh/grammar/hello_worlds/hello_world_%s.txt",args[0]));
-            stream = CharStreams.fromFileName("src/main/java/pl/edu/agh/grammar/second_stage/turkish_march.txt");
+            stream = CharStreams.fromFileName("src/main/java/pl/edu/agh/grammar/second_stage/vivaldi_spring_standard.txt");
 //            stream = CharStreams.fromFileName("src/main/java/pl/edu/agh/grammar/second_stage/turkish_march.txt");
         } catch(Exception e) {
             System.out.println("No such melody!");
