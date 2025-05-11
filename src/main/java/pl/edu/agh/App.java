609dae3fb729621b73455444479f115e5413362d
pavlvs-91 <gleindekp@gmail.com>
2025-05-11 18:25:21 +0200

    Minor function tests improvements

--- src/main/java/pl/edu/agh/App.java
+++ src/main/java/pl/edu/agh/App.java
@@ -23,7 +23,7 @@ public class App
         ImportHandler resolver = new ImportHandler();
         String mergedSource;
         try {
-            mergedSource = resolver.resolveImports("src/main/java/pl/edu/agh/grammar/tests/test17_if_advanced.cb");
+            mergedSource = resolver.resolveImports("src/main/java/pl/edu/agh/grammar/third_stage/beethoven_v.cb");
         } catch (ImportError e) {
             System.out.println(e.getMessage());
             return;
