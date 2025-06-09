89ecbc98c8adb0b61684f0569b76b56f7f86f88b
pavlvs-91 <gleindekp@gmail.com>
2025-06-10 00:32:38 +0200

    Added test programs for final presentation

--- src/main/java/pl/edu/agh/App.java
+++ src/main/java/pl/edu/agh/App.java
@@ -23,7 +23,6 @@ public class App
     public static HashMap<String, VarInfo> globalScope  = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        // String file = "src/main/java/pl/edu/agh/grammar/temp/tmp.cb";
         String file = args[0];
         FilePreImportProcessing analyser = new FilePreImportProcessing();
         try {
