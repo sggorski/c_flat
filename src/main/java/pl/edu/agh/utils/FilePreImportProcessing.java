34c3fb2c2a052ca0af3cc4ba14481c4e20bbabe7
pavlvs-91 <gleindekp@gmail.com>
2025-06-11 12:34:36 +0200

    LineMap bug fixed

--- src/main/java/pl/edu/agh/utils/FilePreImportProcessing.java
+++ src/main/java/pl/edu/agh/utils/FilePreImportProcessing.java
@@ -7,6 +7,9 @@ import pl.edu.agh.MusicErrorListener;
 import pl.edu.agh.MusicLexer;
 import pl.edu.agh.MusicParser;
 import pl.edu.agh.errors.SyntaxError;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,7 +20,7 @@ public class FilePreImportProcessing {
     public void analiseFile(String filename) throws SyntaxError, IOException {
         CharStream startStream = CharStreams.fromFileName(filename);
         Map<Integer, LineOrigin> startLineMap = new HashMap<>();
-        for(int i = 1; i<= Files.readAllLines(Paths.get(filename)).size(); ++i) {
+        for(int i = 1; i<= countLines(filename); ++i) {
             startLineMap.put(i, new LineOrigin(filename, i));
         }
 
@@ -30,5 +33,29 @@ public class FilePreImportProcessing {
         parser.addErrorListener(new MusicErrorListener(startLineMap));
         parser.setErrorHandler(new SuperErrorStrategy());
         parser.program();
+    }
+
+    public static int countLines(String filePath) {
+        int lines = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines++;
+            }
+
+            if (new java.io.RandomAccessFile(filePath, "r").length() > 0) {
+                java.nio.file.Path path = java.nio.file.Paths.get(filePath);
+                byte[] bytes = java.nio.file.Files.readAllBytes(path);
+                if (bytes[bytes.length - 1] == '\n' || bytes[bytes.length - 1] == '\r') {
+                    lines++;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
     }
 }
